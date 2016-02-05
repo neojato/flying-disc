a612e9f095401a0ba75be836f2efdd795d758bfc
Kyle Paul <kpaul86+github@gmail.com>
2016-02-04 22:37:58 -0600

    Add boomerang loader to Speaker list

--- www/js/controllers/speakers.js
+++ www/js/controllers/speakers.js
@@ -2,7 +2,17 @@
   'use strict';
   
   var speakersCtrl = function($scope, SpeakerService) {
+    $scope.loading = true;
+    $scope.listEmpty = false;
     $scope.speakers = SpeakerService;
+    
+    $scope.speakers.$loaded(function(list) {
+      if (list !== $scope.speakers) {
+        $scope.listEmpty = true;
+      }
+      
+      $scope.loading = false;
+    });
   };
 
   var app = angular.module('devfest')
